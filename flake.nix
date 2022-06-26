2c45d3a0a9d9dc683e817e42a631292b78cc9df4
Jeffrey C. Ollie <jeff@ocjtech.us>
2022-06-26 11:13:25 -0500

    clean ups

--- flake.nix
+++ flake.nix
@@ -15,20 +15,23 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        python = pkgs.python310.withPackages (ps: with ps; [
-          poetry
-        ]);
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            python
-          ];
-          shellHook = ''
-            export POETRY_VIRTUALENVS_IN_PROJECT=true
-            export PS1='\n\[\033[1;34m\][aiotftp:\w]\$\[\033[0m\] '
-          '';
-        };
+        devShells.default =
+          let
+            python = pkgs.python310.withPackages (ps: with ps; [
+              poetry
+            ]);
+          in
+          pkgs.mkShell {
+            buildInputs = [
+              python
+            ];
+            shellHook = ''
+              export POETRY_VIRTUALENVS_IN_PROJECT=true
+              export PS1='\n\[\033[1;34m\][aiotftp:\w]\$\[\033[0m\] '
+            '';
+          };
         packages = {
           aiotftp = pkgs.poetry2nix.mkPoetryApplication {
             python = pkgs.python310;
--- setup.py
+++ setup.py
@@ -6,43 +6,35 @@ from setuptools import find_packages, setup
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    return open(file_path, encoding='utf-8').read()
+    return open(file_path, encoding="utf-8").read()
 
 
 setup(
-    name='aiotftp',
-    version='0.2.1',
-    author='Terry Kerr',
-    author_email='terry@dtkerr.ca',
-    maintainer='Terry Kerr',
-    maintainer_email='terry@dtkerr.ca',
+    name="aiotftp",
+    version="0.2.2",
+    author="Terry Kerr",
+    author_email="terry@dtkerr.ca",
+    maintainer="Terry Kerr",
+    maintainer_email="terry@dtkerr.ca",
     packages=find_packages(),
-    license='Mozilla Public License 2.0 (MPL 2.0)',
-    description='Python 3.5+ asyncio TFTP server',
-    long_description=read('README.rst'),
-    install_requires=[
-        'attrs',
-        'async-timeout'
-    ],
-    tests_require=[
-        'async-generator',
-        'hypothesis',
-        'pytest',
-        'pytest-asyncio'
-    ],
-    setup_requires=['setuptools>=17.1'],
+    license="Mozilla Public License 2.0 (MPL 2.0)",
+    description="Python 3.5+ asyncio TFTP server",
+    long_description=read("README.rst"),
+    install_requires=["attrs", "async-timeout"],
+    tests_require=["async-generator", "hypothesis", "pytest", "pytest-asyncio"],
+    setup_requires=["setuptools>=17.1"],
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Development Status :: 4 - Beta',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Framework :: AsyncIO',
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Framework :: AsyncIO",
     ],
 )
